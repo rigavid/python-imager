e622885aa6171a52106cb9c82b19376b866a2f20
T-Sana <110158900+T-Sana@users.noreply.github.com>
2024-10-19 14:15:59 +0200

    Fixing errors

--- pyimg/resolution.py
+++ pyimg/resolution.py
@@ -5,7 +5,7 @@ monitors = [m for m in get_monitors()]
 class resolution:
     resolutions = [(m.width, m.height) for m in monitors]
     index = [m.is_primary for m in monitors].index(True)
-    resolution = resolution[index]
+    resolution = resolutions[index]
     def update():
         resolution.index = (resolution.index+1)%len(resolution.resolutions)
         resolution.resolution = resolution.resolutions[resolution.index]
