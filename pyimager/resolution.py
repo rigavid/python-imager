69270d94a54e626480710a2e3ca5d18263448323
T-Sana <tim.tamet@gmail.com>
2024-11-18 18:58:17 +0100

    Fixed exception when only one screen is connected

--- pyimager/resolution.py
+++ pyimager/resolution.py
@@ -4,7 +4,8 @@ monitors = [m for m in get_monitors()]
 
 class resolution:
     resolutions = [(m.width, m.height) for m in monitors]
-    index = [m.is_primary for m in monitors].index(True)
+    try: index = [m.is_primary for m in monitors].index(True)
+    except: index = 0
     resolution = resolutions[index]
     def update():
         resolution.index = (resolution.index+1)%len(resolution.resolutions)
