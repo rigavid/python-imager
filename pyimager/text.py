c64cc9475add47f008921adbe805b5f250a85631
T-Sana <110158900+T-Sana@users.noreply.github.com>
2025-01-07 19:06:29 +0100

    Starting the writing program

--- pyimager/text.py
+++ pyimager/text.py
@@ -1,15 +1,30 @@
 from pyimager.main import *
 
-## TODO
+## TODO ##
+## Format char : LLXX ##
+CONV = {
+    "AA00":"a", "AA01":"b" ## Re-checker le vieux carnet de prog et le prog cvt.py
+}
 
 class Text:
-    def encode(self, t): return t
-    def decode(self, t): return t
+    class chain:
+        def __init__(self, string=""):
+            self.string = string
+            self.chain = self.enchain()
+        def enchain(self):
+            for char in self.string:...
+
+        def __str__(self):
+            return self.string
+
     def __init__(self, text):
-        self.text = self.encode(text)
+        self.text = self.chain(text)
+
     def __eq__(self, other):
         if type(self) == type(other):
             return self.text == other.text
         else:
             return self.decode(self.text) == str(other)
-    def __str__(self): return self.decode(self.text)
+
+    def __str__(self):
+        return self.text.__str__()
