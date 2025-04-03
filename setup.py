402af1f63a8165c4f6ad34791c440be036ddba79
Rigavid <110158900+rigavid@users.noreply.github.com>
2025-04-03 19:07:13 +0200

    Fixing identation

--- setup.py
+++ setup.py
@@ -9,19 +9,19 @@ with open("README.md", "r", encoding="utf8") as file:
     LONG_DESCRIPTION = file.read()
 
 setup(
-        name="python-imager",
-        version=VERSION,
-        author="Rigavid",
-        author_email="tsana.code@gmail.com",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        long_description_content_type='text/markdown',
-        packages=find_packages(),
-        install_requires=["numpy", "opencv-python", "screeninfo"],
-        keywords=['python', 'image'],
-        classifiers= [
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-        ],
-        license="LICENCE",
+    name="python-imager",
+    version=VERSION,
+    author="Rigavid",
+    author_email="tsana.code@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=["numpy", "opencv-python", "screeninfo"],
+    keywords=['python', 'image'],
+    classifiers= [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    license="LICENCE",
 )
