f2355dd51f45145dafbff37c937670ae77153745
Rigavid <110158900+rigavid@users.noreply.github.com>
2025-04-13 22:07:17 +0200

    Updated to pyproject.toml

--- TODO.md
+++ TODO.md
@@ -1,5 +1,3 @@
-[ ] Check this out: https://packaging.python.org/en/latest/guides/modernize-setup-py-project/
-
 [x] Permit percentiles in coordinates arguments as image.rectangle([20%, 20%], [80%, 80%], COL.red, 0) -> use RES.percentile(x, y)
 
 [ ] Update README.md
@@ -7,5 +5,3 @@
 [ ] Implement Alignement in Text.draw() (Left, right, center)
 
 [ ] Implement ligatures in Text() and draw them
-
-[x] Create sliders on images as buttons (dont just use cv2.CreateTrackBar() and so)
--- setup.py
+++ setup.py
@@ -1,28 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# python3 -m build
-# twine upload  dist/* --verbose
-# pip install --upgrade python-imager
-
-VERSION = '1.2.40'
-DESCRIPTION = 'Python image program'
-with open("README.md", "r", encoding="utf8") as file:
-    LONG_DESCRIPTION = file.read()
-
-setup(
-    name="python-imager",
-    version=VERSION,
-    author="Rigavid",
-    author_email="tsana.code@gmail.com",
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    packages=find_packages(),
-    install_requires=["numpy", "opencv-python", "screeninfo"],
-    keywords=['python', 'image'],
-    classifiers= [
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-    ],
-    license="LICENCE",
-)
+if __name__ == "__main__":
+    setup()
